0bc1cc16bea7f5a00c9e67ffff70279e8be934ee
JinWYP <jinwyp@gmail.com>
2020-07-31 23:05:24 +0800

    U-update

--- favorite.js
+++ favorite.js
@@ -1,5 +1,3 @@
-
-
 const userListElement = document.querySelector("#post-list");
 const buttonSearchElement = document.querySelector("#btn-search");
 const buttonClearTagElement = document.querySelector("#btn-cleartag");
@@ -93,7 +91,7 @@ function getChromeData() {
 
 
 
-function showSelectOptions () {
+function showSelectOptions() {
     $("#searchtag").empty();
 
     const opt = document.createElement('option');
@@ -101,7 +99,7 @@ function showSelectOptions () {
     opt.innerHTML = '全部';
     selectSearchElement.appendChild(opt);
 
-    tagList.forEach( (tag) => {
+    tagList.forEach((tag) => {
         const opt = document.createElement('option');
         opt.value = tag;
         opt.innerHTML = tag;
@@ -127,37 +125,37 @@ function showList(uuid) {
     }
 
     $.ajax({
-        url: apiPrefix + "/api/tgfcer/user/favorite",
-        method: "GET",
-        data: tempQuery,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json"
-    }).done( (data) => {
-        if (Array.isArray(data.data)) {
-            userFavoriteLinkList = data.data;
+            url: apiPrefix + "/api/tgfcer/user/favorite",
+            method: "GET",
+            data: tempQuery,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done((data) => {
+            if (Array.isArray(data.data)) {
+                userFavoriteLinkList = data.data;
 
-            let tempHtml = '';
+                let tempHtml = '';
 
-            userFavoriteLinkList.forEach((post) => {
+                userFavoriteLinkList.forEach((post) => {
 
-                if (uuid && uuid === post.uuid) {
-                    tempHtml = tempHtml + `<li class="list-group-item" id="id-${post.uuid}"> <span class="float-left mr-2">${post.threadTitle}</span> <input type="text" class="form-control col-sm-4 float-left" id="inputtag-${post.uuid}" value="${post.threadTag}" placeholder="请填写分类"> <input type="text" class="form-control col-sm-4 float-left ml-1" id="inputremark-${post.uuid}" value="${post.remark}" placeholder="请填写备注"> <button class="btn btn-outline-primary btn-sm ml-2 float-left" id="save-${post.uuid}">保存修改</button> </li>`
-                } else {
-                    tempHtml = tempHtml + `<li class="list-group-item" id="id-${post.uuid}"> ${post.threadTitle} | 分类: ${post.threadTag || '暂无'}  | 备注: ${post.remark || '暂无'} <button class="btn btn-outline-primary btn-sm ml-4" id="edit-${post.uuid}">编辑</button> <button class="btn btn-outline-danger btn-sm" id="dele-${post.uuid}">删除</button> </li>`
-                }
-            })
+                    if (uuid && uuid === post.uuid) {
+                        tempHtml = tempHtml + `<li class="list-group-item" id="id-${post.uuid}"> <span class="float-left mr-2">${post.threadTitle}</span> <input type="text" class="form-control col-sm-4 float-left" id="inputtag-${post.uuid}" value="${post.threadTag}" placeholder="请填写分类"> <input type="text" class="form-control col-sm-4 float-left ml-1" id="inputremark-${post.uuid}" value="${post.remark}" placeholder="请填写备注"> <button class="btn btn-outline-primary btn-sm ml-2 float-left" id="save-${post.uuid}">保存修改</button> </li>`
+                    } else {
+                        tempHtml = tempHtml + `<li class="list-group-item" id="id-${post.uuid}"> <a target="_blank" href="${post.url}">${post.threadTitle}</a> | 分类: ${post.threadTag || '暂无'}  | 备注: ${post.remark || '暂无'} <button class="btn btn-outline-primary btn-sm ml-4" id="edit-${post.uuid}">编辑</button> <button class="btn btn-outline-danger btn-sm" id="dele-${post.uuid}">删除</button> </li>`
+                    }
+                })
 
-            userListElement.innerHTML = tempHtml;
-        }
+                userListElement.innerHTML = tempHtml;
+            }
 
-        // console.log('jQuery Ajax success!')
-    })
-    .fail( () => {
-        console.log('jQuery Ajax error!')
-    })
-    .always( () => {
-        // console.log('jQuery Ajax complete!')
-    });
+            // console.log('jQuery Ajax success!')
+        })
+        .fail(() => {
+            console.log('jQuery Ajax error!')
+        })
+        .always(() => {
+            // console.log('jQuery Ajax complete!')
+        });
 
 
 }
@@ -195,25 +193,25 @@ function onclickEditButton(event1) {
                 userFavoriteLinkList.splice(indexUser, 1);
 
                 $.ajax({
-                    url: apiPrefix + "/api/tgfcer/user/favorite/" + isExist.uuid,
-                    method: "DELETE",
-                    data: JSON.stringify({
-                        token: getToken(26),
-                        uuid: isExist.uuid,
-                        submitUserId: currentUserId,
-                        threadId: isExist.threadId,
-                    }),
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json"
-                }).done( (data) => {
-                    showList();
-                    // console.log('jQuery Ajax success!')
-                }).fail( () => {
-                    console.log('jQuery Ajax error!')
-                })
-                .always( () => {
-                    // console.log('jQuery Ajax complete!')
-                });
+                        url: apiPrefix + "/api/tgfcer/user/favorite/" + isExist.uuid,
+                        method: "DELETE",
+                        data: JSON.stringify({
+                            token: getToken(26),
+                            uuid: isExist.uuid,
+                            submitUserId: currentUserId,
+                            threadId: isExist.threadId,
+                        }),
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json"
+                    }).done((data) => {
+                        showList();
+                        // console.log('jQuery Ajax success!')
+                    }).fail(() => {
+                        console.log('jQuery Ajax error!')
+                    })
+                    .always(() => {
+                        // console.log('jQuery Ajax complete!')
+                    });
 
             } else {
 
@@ -253,12 +251,12 @@ function onclickEditButton(event1) {
                         }),
                         contentType: "application/json; charset=utf-8",
                         dataType: "json"
-                    }).done( (data) => {
+                    }).done((data) => {
                         showList();
                         // console.log('jQuery Ajax success!')
-                    }).fail( () => {
+                    }).fail(() => {
                         console.log('jQuery Ajax error!')
-                    }).always( () => {
+                    }).always(() => {
                         // console.log('jQuery Ajax complete!')
                     });
                 }
@@ -271,14 +269,14 @@ function onclickEditButton(event1) {
 userListElement.addEventListener('click', onclickEditButton, false);
 
 
-function onClickSearchButton (event3) {
+function onClickSearchButton(event3) {
     event3.preventDefault()
     showList()
 }
 buttonSearchElement.addEventListener('click', onClickSearchButton, false);
 
 
-function onClickClearTagButton (event4) {
+function onClickClearTagButton(event4) {
     event4.preventDefault()
     tagList = []
     showSelectOptions();
