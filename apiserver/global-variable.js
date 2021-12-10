a6530ea2809adf6617b1b694d6fe19af31e5bd1b
JinWYP <jinwyp@gmail.com>
2021-12-11 02:14:35 +0800

    U-update

--- apiserver/global-variable.js
+++ apiserver/global-variable.js
@@ -4,7 +4,7 @@ global.GDirUtil = require('./koa2/common-libs/create-directory')
 global.GConfig = require('./config')
 global.GLogger = require('./koa2/koa2-middleware/logger-log4js').logger
 global.GProjectPath = process.cwd()
-console.log(process.cwd())
+console.log("===== Project Root Path: ", process.cwd())
 
 
 global.GSystemError = require('./koa2/errors/SystemError')
